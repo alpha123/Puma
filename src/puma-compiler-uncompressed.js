4eec892c236b950ec96ae2340815af3e4fa9877f
Peter Cannici <turkchess123@gmail.com>
2011-06-30 21:24:45 -0600

    free the compiled output from all those useless immediately executing anonymous functions

--- src/puma-compiler-uncompressed.js
+++ src/puma-compiler-uncompressed.js
@@ -1,5 +1,11 @@
 (function (Puma, document, undefined) {
 
+function compileBranch(branch) {
+    if (branch.arity == 'binary')
+        return '(function(){' + Puma.Compiler.compile(branch, true).replace('~e', 'return') + '})()';
+    return Puma.Compiler.compile(branch, true).replace('~e', '');
+}
+
 Puma.Compiler = {
     cache: [],
     cacheSize: 30,
@@ -7,14 +13,14 @@ Puma.Compiler = {
     canSize: 100,
     
     compiled: {
-        'unary#': 'return[c.getElementById("~v")]',
-        'unary.': document.getElementsByClassName ? 'return[].slice.call(c.getElementsByClassName("~v"))' :
-          'return Puma.f(c.getElementsByTagName("*"),function(e){return Puma.i(e.className.split(" "),"~v")>-1})',
-        'binary#': 'var l=~l;return Puma.f(c.getElementsByTagName("*"),function(e){return e.id=="~v"&&Puma.i(l,e)>-1})',
+        'unary#': '~e[c.getElementById("~v")]',
+        'unary.': document.getElementsByClassName ? '~e[].slice.call(c.getElementsByClassName("~v"))' :
+          '~e Puma.f(c.getElementsByTagName("*"),function(e){return Puma.i(e.className.split(" "),"~v")>-1})',
+        'binary#': 'var l=~l;~e Puma.f(c.getElementsByTagName("*"),function(e){return e.id=="~v"&&Puma.i(l,e)>-1})',
         'binary.': document.getElementsByClassName ?
-          'var l=~l;return Puma.f(c.getElementsByClassName("~v"),function(e){return Puma.i(l,e)>-1})' :
-          'var l=~l;return Puma.f(c.getElementsByTagName("*"),function(e){return Puma.i(e.className.split(" "),"~v")>-1&&Puma.i(l,e)>-1})',
-        'binary ': 'var l=~l;return Puma.f(~r,function(e){var p=e;while(p=p.parentNode)if(Puma.i(l,p)>-1)return 1;return 0})'
+          'var l=~l;~e Puma.f(c.getElementsByClassName("~v"),function(e){return Puma.i(l,e)>-1})' :
+          'var l=~l;~e Puma.f(c.getElementsByTagName("*"),function(e){return Puma.i(e.className.split(" "),"~v")>-1&&Puma.i(l,e)>-1})',
+        'binary ': 'var l=~l;~e Puma.f(~r,function(e){var p=e;while(p=p.parentNode)if(Puma.i(l,p)>-1)return 1;return 0})'
     },
     
     canCompile: function (tree) {
@@ -35,18 +41,15 @@ Puma.Compiler = {
         if (this.cache[tree.query])
             return this.cache[tree.query];
         var fn, func, isSane = document.body instanceof Object;
-        if (tree instanceof Puma.AST.UnOp)
-            fn = this.compiled['unary' + tree.value].replace('~v', tree.right.value);
-        else if (tree instanceof Puma.AST.BinOp) {
-            fn = this.compiled['binary' + tree.value]
-              .replace('~v', tree.right.value)
-              .replace('~l', '(function(){' + this.compile(tree.left, true) + '})()')
-              .replace('~r', '(function(){' + this.compile(tree.right, true) + '})()');
-        }
-        else // Puma.AST.Tag
-            fn = (isSane ? 'return[].slice.call(' : 'return Puma.f(') + 'c.getElementsByTagName("' +
+        if (tree.arity == 'ident')
+            fn = (isSane ? '~e[].slice.call(' : '~e Puma.f(') + 'c.getElementsByTagName("' +
             tree.value + '")' + (isSane ? ')' : ',function(){return 1})');
-        func = noFn ? fn : Function('c', fn);
+        else {
+            fn = this.compiled[tree.arity + tree.value].replace('~v', tree.right.value);
+            if (tree.arity == 'binary')
+                fn = fn.replace('~l', compileBranch(tree.left)).replace('~r', compileBranch(tree.right));
+        }
+        func = noFn ? fn : Function('c', fn.replace('~e', 'return'));
         if (!noFn) {
             this.cache.push(tree.query);
             this.cache[tree.query] = func;
