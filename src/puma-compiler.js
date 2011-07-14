c3cca4004f1b94ee9dd48148fd9b3242b331a6b1
Peter Cannici <turkchess123@gmail.com>
2011-07-14 11:27:00 -0600

    fixed noConflict

--- src/puma-compiler.js
+++ src/puma-compiler.js
@@ -19,26 +19,26 @@ Puma.Compiler = {
         'ident': function (value) {
             if (document.body instanceof Object) // Can't check earlier because the DOM might not be loaded
                 return '#r[].slice.call(c.getElementsByTagName("' + value + '"))';
-            return '#r Puma.f(c.getElementsByTagName("' + value + '"),function(){return 1})';
+            return '#r P.f(c.getElementsByTagName("' + value + '"),function(){return 1})';
         },
         'unary#': function (value) { return '#r[c.getElementById("' + value + '")]'; },
         'unary.': function (value) {
             return byClass ? '#r[].slice.call(c.getElementsByClassName("' + value + '"))' :
-            '#r Puma.f(c.getElementsByTagName("*"),function(e){return Puma.i(e.className.split(" "),"' + value + '")>-1})';
+            '#r P.f(c.getElementsByTagName("*"),function(e){return P.i(e.className.split(" "),"' + value + '")>-1})';
         },
         'binary#': function (value, left, _, leftBranch) {
             if (leftBranch.arity == 'ident') // Do a little optimization
                 return 'var e=c.getElementById("' + value + '");#r e&&e.tagName.toUpperCase()=="' + leftBranch.value.toUpperCase() + '"?[e]:[]';
-            return 'var l=' + left + ';#r Puma.f(c.getElementsByTagName("*"),function(e){return e.id=="' + value + '"&&Puma.i(l,e)>-1})';
+            return 'var l=' + left + ';#r P.f(c.getElementsByTagName("*"),function(e){return e.id=="' + value + '"&&P.i(l,e)>-1})';
         },
         'binary.': function (value, left, _, leftBranch) {
             if (leftBranch.arity == 'ident' && byClass)
-                return '#r Puma.f(c.getElementsByClassName("' + value +
+                return '#r P.f(c.getElementsByClassName("' + value +
                   '"),function(e){return e.tagName.toUpperCase()=="' + leftBranch.value.toUpperCase() + '"})';
             return byClass ?
-              'var l=' + left + ';#r Puma.f(c.getElementsByClassName("' + value + '"),function(e){return Puma.i(l,e)>-1})' :
-              'var l=' + left + ';#r Puma.f(c.getElementsByTagName("*"),function(e){return Puma.i(e.className.split(" "),"' +
-              value + '")>-1&&Puma.i(l,e)>-1})'
+              'var l=' + left + ';#r P.f(c.getElementsByClassName("' + value + '"),function(e){return P.i(l,e)>-1})' :
+              'var l=' + left + ';#r P.f(c.getElementsByTagName("*"),function(e){return P.i(e.className.split(" "),"' +
+              value + '")>-1&&P.i(l,e)>-1})'
         },
         'binary ': function (value, left, right, leftBranch, rightBranch) {
             if (leftBranch.arity != 'binary' && document.body instanceof Object) {
@@ -48,7 +48,7 @@ Puma.Compiler = {
                 else if (rightBranch.arity == 'unary' && rightBranch.value == '.' && byClass)
                     return str + 'getElementsByClassName("' + rightBranch.right.value + '")));#r n';
             }
-            return 'var l=' + left + ';#r Puma.f(' + right + ',function(e){var p=e;while(p=p.parentNode)if(Puma.i(l,p)>-1)return 1;return 0})'
+            return 'var l=' + left + ';#r P.f(' + right + ',function(e){var p=e;while(p=p.parentNode)if(P.i(l,p)>-1)return 1;return 0})'
         }
     },
     
@@ -69,7 +69,9 @@ Puma.Compiler = {
     compile: function (tree, noFn) {
         if (this.cache[tree.query])
             return this.cache[tree.query];
-        var fn, func;
+        var fn, func, wrapped = function (context) {
+            return func(context, Puma);
+        };
         if (tree.arity == 'ident')
             fn = this.compiled['ident'](tree.value);
         else if (tree.arity == 'unary')
@@ -77,14 +79,14 @@ Puma.Compiler = {
         else
             fn = this.compiled['binary' + tree.value](tree.right.value, compileBranch.call(this, tree.left),
               compileBranch.call(this, tree.right), tree.left, tree.right);
-        func = noFn ? fn : Function('c', fn.replace('#r', 'return'));
+        func = noFn ? fn : Function('c,P', fn.replace('#r', 'return'));
         if (!noFn) {
             this.cache.push(tree.query);
-            this.cache[tree.query] = func;
+            this.cache[tree.query] = wrapped;
             if (this.cache.length > this.cacheSize)
                 this.cache[this.cache.shift()] = undefined;
         }
-        return func;
+        return wrapped;
     }
 };
 
